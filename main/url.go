ac631e9d9ae718307df691ef495704954baf53b6
Arrianne S <asalvador@zvelo.com>
2014-09-18 09:11:29 +0800

    Updated main script

--- main/url.go
+++ main/url.go
@@ -16,18 +16,25 @@ func main() {
 		return
 	}
 
-	url := gourl.NewURL(u)
+	url, err := gourl.Parse(u)
+
+	if err != nil {
+		fmt.Println("error enountered while parsing", u)
+		return
+	}
 
 	fmt.Println("URL: ", url.URL)
-	fmt.Println("Scheme: ", url.Scheme())
-	fmt.Println("User: ", url.User())
-	fmt.Println("Password: ", url.Password())
-	fmt.Println("Subdomain: ", url.Subdomain())
-	fmt.Println("Domain: ", url.Domain())
-	fmt.Println("Hostname: ", url.Hostname())
-	fmt.Println("Path: ", url.Path())
-	fmt.Println("Query: ", url.Query())
-	fmt.Println("Fragment: ", url.Fragment())
+	fmt.Println("Normalized URL: ", url.String())
+	fmt.Println("Scheme: ", url.Scheme)
+	fmt.Println("User: ", url.User)
+	fmt.Println("Password: ", url.Password)
+	fmt.Println("Subdomain: ", url.Subdomain)
+	fmt.Println("Domain: ", url.Domain)
+	fmt.Println("Hostname: ", url.Hostname)
+	fmt.Println("Port: ", url.Port)
+	fmt.Println("Path: ", url.Path)
+	fmt.Println("Query: ", url.Query)
+	fmt.Println("Fragment: ", url.Fragment)
 
 	return
 }
